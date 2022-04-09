4427c6ec4461022dac3cd7cc09e8d9f0ccfc6f71
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-09 14:34:58 -0400

    add tables and seeds for database

---
+++ db/schema/04_resource_reviews.sql
@@ -0,0 +1,11 @@
+-- Drop and recreate Resource Review table (Example)
+
+DROP TABLE IF EXISTS resource_reviews CASCADE;
+CREATE TABLE resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) NOT NULL,
+  resource_id INTEGER REFERENCES resources(id) NOT NULL,
+  comment TEXT,
+  rating INTEGER,
+  liked BOOLEAN DEFAULT FALSE
+);
---
+++ db/seeds/02_resources.sql
@@ -0,0 +1,12 @@
+-- Resources table seeds here
+
+INSERT INTO resources (owner_id, title, description, topic, external_url)
+VALUES (1, 'Summer drink', 'How to make frozens margaritas', 'diy', 'https://www.isabeleats.com/frozen-margarita-recipe/'),
+(2, 'Make your own scrunchies', 'How to make scrunchies', 'diy', 'https://www.youtube.com/watch?v=MW1GiQseu8E'),
+(3, 'Space tattoo', 'Idea for a space tattoo', 'tattoo', 'https://www.pinterest.ca/pin/177399672800127661/'),
+(4, 'Wrist tattoos', 'Ideas for wirst tattoos', 'tattoo', 'https://www.cosmopolitan.com/style-beauty/beauty/g27466915/wrist-tattoos-ideas-designs/'),
+(5, 'Mountain Village', 'Image credit Matej Jan', 'art', 'https://cdn.mos.cms.futurecdn.net/jbCNvTM4gwr2qV8X8fW3ZB.png'),
+(6, 'Peaceful Valley', 'Image credit Jubilee', 'art', 'https://cdn.mos.cms.futurecdn.net/EFXSes9UCfsyRVoNeQ2ZTB-970-80.png.webp'),
+(7, 'Workouts for men', 'A guide to workout for men', 'guides', 'https://www.muscleandstrength.com/workouts/men'),
+(8, 'Easter Eggs GTA V', 'Where to find all the easter eggs in GTA V', 'guides', 'https://screenrant.com/grand-theft-auto-hidden-tips-hints-gta-5/'),
+(9,)
