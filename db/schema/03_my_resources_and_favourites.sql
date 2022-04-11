b18280c388c39c5faa20623b65bde0f53e813835
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-11 09:11:56 -0400

    change table name to correct name

--- db/schema/03_my_resources_and_favourites.sql
+++ db/schema/03_my_resources_and_favourites.sql
@@ -1,7 +1,7 @@
 -- Drop and recreate Favourites table (Example)
 
 DROP TABLE IF EXISTS favourites CASCADE;
-CREATE TABLE resources (
+CREATE TABLE favourites (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) NOT NULL,
   resource_id INTEGER REFERENCES resources(id) NOT NULL
