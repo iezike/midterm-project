ed03cd027e70eef33bd1ca56dee532fce9e50766
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-12 19:38:57 -0400

    more work on favourites page

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const bcrypt = require("bcryptjs");
-const { getUserId, getUserByEmail } = require('../helpers');
+// const { getUserId, getUserByEmail } = require('../helpers');
 
 module.exports = (db, dbQueries) => {
   router.get("/", (req, res) => {
--- routes/my_favourites.js
+++ routes/my_favourites.js
@@ -1,18 +1,29 @@
 const express = require('express');
+const login = require('./login');
 const router = express.Router();
 
 
 module.exports = (db) => {
+  const getComments = (resourceId) => {
+    const stringParams = ` SELECT comment, users.name, resource_id
+    FROM resource_reviews
+    JOIN users ON users.id = user_id
+    WHERE resource_id = $1
+    `
+    return db.query(stringParams, [resourceId])
+  }
+
+
   const getMyResources = (userID) => {
     let queryString = `
-    SELECT DISTINCT resources.*, avg(rating) as average_rating
+    SELECT DISTINCT resources.*, avg(rating) as rating
     FROM resources
     JOIN users ON owner_id = users.id
     JOIN favourites ON favourites.resource_id = resources.id
     JOIN resource_reviews ON resource_reviews.user_id = users.id
     WHERE favourites.user_id = $1
     AND LIKED IS true
-    group by resources.id
+    GROUP BY resources.id
     `;
     const queryParams = [userID];
 
@@ -23,20 +34,14 @@ module.exports = (db) => {
     const owner = req.session.userID
     getMyResources(owner)
     .then(results => {
-      // console.log(' does this work',results.rows);
-      const templateVars = {info: results.rows}
-      console.log('jsdvfsdhvf', templateVars);
-      res.render('my_favourites', templateVars);
+        res.render('my_favourites', {results});
       })
+      // const templateVars = {info: results.rows}
+      // console.log('jsdvfsdhvf', templateVars);
+      
   });
 
-  // router.post('/', (req, res) => {
-  //   const owner = req.session.userID
-  //   getMyResources(owner)
-  //   .then(result => {
-  //     console.log(result.rows[0]);
-  //     res.send({result})
-  //   })
+
 
   return router;
 };
