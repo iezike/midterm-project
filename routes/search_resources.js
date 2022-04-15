cf6c633e820df3504f34a3cdb6c833c6a7b22f6e
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-15 09:29:18 -0400

    bug

--- routes/search_resources.js
+++ routes/search_resources.js
@@ -7,16 +7,48 @@ const router = express.Router();
 
 // Route to handle a user registeration form
 module.exports = function (db) {
+  const getLikes = (resourceLike) => {
+    const stringParams = ` SELECT resource_id, like_count as likes
+    FROM resource_reviews
+    JOIN resources ON resources.id = resource_id
+    WHERE resource_id = $1
+    `;
+    return db.query(stringParams, [resourceLike]).then(res => res.rows);
+  };
+
+  const getSingleRequest = (id) => {
+    const queryString = `
+    SELECT resources.*, avg(rating) as rating
+    FROM resources
+    LEFT JOIN resource_reviews ON resource_reviews.resource_id = resources.id
+    WHERE resources.id = $1
+    GROUP BY resources.id
+    `;
+    return db.query(queryString, [id]).then(res => res.rows);
+  };
+
+  const getComments = (resourceId) => {
+    const stringParams = ` SELECT comment, users.name, resource_id
+    FROM resource_reviews
+    JOIN users ON users.id = user_id
+    WHERE resource_id = $1
+    ORDER BY resource_reviews.id DESC
+    `;
+    return db.query(stringParams, [resourceId]).then(res => res.rows);
+  };
+
+
+
   const getSearchData = (text) => {
     let resourceQuery = `SELECT title, description, topic, external_url, avg(rating) as rating
     FROM resources
-    JOIN resource_reviews ON  resources.id = resource_id
+    LEFT JOIN resource_reviews ON  resources.id = resource_id
     WHERE description LIKE $1 OR title LIKE $1 OR topic LIKE $1
     GROUP BY resources.id
     `;
     return db.query(resourceQuery, [`%${text}%`])
   };
-  
+
   const getUserName = (userID) => {
     const userNameQuery = ` SELECT name
     FROM users
@@ -51,5 +83,21 @@ module.exports = function (db) {
           })
       })
   })
+
+  router.get('/:resource_id', (req, res) => {
+    let id = req.params.resource_id;
+    let user = req.session.userID;
+    let likes = req.params.like_count;
+    // add like grab function inside here
+    Promise
+      .all([getSingleRequest(id), getComments(id), getLikes(likes), getUserName(user)])
+      .then(([resultData, resultComments, resultLikes, activeUser]) => {
+        const data = resultData[0];
+        const comments = resultComments;
+        const likes = resultLikes;
+        res.render('test', { data, comments, likes, id, activeUser });
+      });
+  });
+
   return router;
 }
